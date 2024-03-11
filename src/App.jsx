f0f0b2aa9e94b43228e1bcf1a2c2323dd50cf778
julia.paulin <julia.paulin@chasacademy.se>
2024-03-11 13:05:08 +0100

    part1

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+//import { useState } from 'react'
+import "./App.css";
+import { jobs } from "./jobs.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Search />
+      <main>
+        {jobs.map((job) => {
+          return (
+            <Card
+              key={job.id}
+              position={job.position}
+              location={job.location}
+              contract={job.contract}
+              logo={job.logo}
+            />
+          );
+        })}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Jobchaser</h1>
+    </header>
+  );
+}
+
+function Search() {
+  return (
+    <form className="search">
+      <input type="text" placeholder="Search" />
+      <button id="search-button">Search</button>
+    </form>
+  );
+}
+
+function Card({ position, location, contract, logo }) {
+  // if ((description, city)) {
+  //   console.log(description, city);
+  // }
+  return (
+    <article className="card" style={{ borderRadius: "20px" }}>
+      <h2 className="card-title">{position}</h2>
+      <div className="card-info">
+        <p>{location}</p>
+        <p>{contract}</p>
+      </div>
+      <img className="card-logo" alt="logo" src={logo} />
+    </article>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>jobchaser.se</p>
+    </footer>
+  );
+}
+
+export default App;
