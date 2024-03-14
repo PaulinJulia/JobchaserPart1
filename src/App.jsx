5e5ee3b59d75c326f74ece6b04cf26486aea5fcb
julia.paulin <julia.paulin@chasacademy.se>
2024-03-14 20:29:43 +0100

    worked on search

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,24 @@
-//import { useState } from 'react'
+import { useState } from "react";
 import "./App.css";
 import { jobs } from "./jobs.jsx";
 
 function App() {
-  const numJobs = jobs.length;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const searched = jobs.filter((job) =>
+    job.position.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <Header />
-      <Search />
+      <Search onSearch={handleChange} searchTerm={searchTerm} />
       <main>
-        {numJobs > 0 ? (
-          <ul className="job-list">
-            {jobs.map((job) => {
-              return (
-                <Card
-                  key={job.id}
-                  position={job.position}
-                  location={job.location}
-                  contract={job.contract}
-                  logo={job.logo}
-                />
-              );
-            })}
-          </ul>
-        ) : (
-          <p>No jobs are available at the moment</p>
-        )}
+        <JobList jobs={searched} />
       </main>
       <Footer />
     </div>
@@ -41,51 +34,31 @@ function Header() {
 }
 
 function Search() {
-  const [SearchTerm, setSearchTerm] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Searching...");
-    //Filtrera ut jobben. Nu görs inget här.
-    const searched = jobs.filter((job) => {
-      job.title.toLowerCase().includes(SearchTerm.toLowerCase());
-      return (
-        <Card
-          key={job.id}
-          position={job.position}
-          location={job.location}
-          contract={job.contract}
-          logo={job.logo}
-        />
-      );
-    });
-  };
-  const handleChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
   return (
-    <form className="search" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        onChange={handleChange}
-        value={SearchTerm}
-        placeholder="Search"
-      />
+    <form>
+      <label htmlFor="search">Search: </label>
+      <input type="text" onChange={handleChange} value={SearchTerm} />
       <button id="search-button">Search</button>
     </form>
   );
 }
 
-function Card({ position, location, contract, logo }) {
+function JobList({ jobs }) {
   return (
-    <article className="card" style={{ borderRadius: "20px" }}>
-      <h2 className="card-title">{position}</h2>
-      <div className="card-info">
-        <p>{location}</p>
-        <p>{contract}</p>
-      </div>
-      <img className="card-logo" alt="logo" src={logo} />
-    </article>
+    <ul className="job-list">
+      {jobs.map((job) => {
+        return (
+          <article className="card" style={{ borderRadius: "20px" }}>
+            <h2 className="card-title">{job.position}</h2>
+            <div className="card-info">
+              <p>{job.location}</p>
+              <p>{job.contract}</p>
+            </div>
+            <img className="card-logo" alt="logo" src={job.logo} />
+          </article>
+        );
+      })}
+    </ul>
   );
 }
 
